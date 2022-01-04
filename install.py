297f35c2d4cb9f2660d3e461c873d1a3f4676ab5
Bob Callaway <bob.callaway@gmail.com>
2022-01-04 14:55:46 -0500

    normalize paths
    Signed-off-by: Bob Callaway <bob.callaway@gmail.com>

--- install.py
+++ install.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# the goal is to install this using core python libs with NO external deps
 import hashlib
 import logging
 import os
@@ -108,10 +109,17 @@ if len(sys.argv) != 2:
     log_and_exit("invalid number of arguments specified")
 
 # check for installation directory value
-install_dir=sys.argv[1]
-install_dir_stat = os.stat(install_dir)
-if not stat.S_ISDIR(install_dir_stat.st_mode):
-    log_and_exit("installation path is not a directory, exiting")
+install_dir=os.path.normpath(sys.argv[1])
+try:
+    install_dir_stat = os.stat(install_dir)
+except FileExistsError:
+    logger.debug(f"installation directory '{ install_dir }' already exists")
+    if not stat.S_ISDIR(install_dir_stat.st_mode):
+        log_and_exit("installation path is not a directory, exiting")
+except FileNotFoundError:
+    # create the installation directory if it doesn't exist already
+    os.mkdir(install_dir)
+    logger.debug(f"created installation directory at { install_dir }")
 
 # source bootstrap_version.txt
 logger.info('reading in bootstrap information...')
@@ -123,20 +131,12 @@ if not bootstrap_version:
 
 logger.info(f"bootstrap version '{ bootstrap_version }' of cosign requested")
 
-
 # ensure the cosign release version requested is well-formed
 cosign_release=os.getenv('COSIGN_RELEASE', "")
 if not re.match(r"v([0-9]+\.){0,2}(\*|[0-9]+)$", cosign_release):
     log_and_exit(f"invalid cosign release '{ cosign_release }' requested")
 elif cosign_release != bootstrap_version:
     logger.info(f"requesting custom cosign release '{ cosign_release }'")
-
-# create the installation directory if it doesn't exist already
-try:
-    os.mkdir(install_dir)
-    logger.debug(f"created installation directory at { install_dir }")
-except FileExistsError:
-    logger.debug(f"installation directory '{ install_dir }' already exists")
 
 # determine the file names based on OS and processor architecture
 OS=os.getenv('RUNNER_OS')
@@ -200,7 +200,7 @@ else:
     log_and_exit(f"Runner OS detected '{OS}' is not supported!")
 
 # download bootstrap version of cosign, also verify SHA against externally set values
-bootstrap_file_with_path=f"{os.getenv('INSTALL_DIR')}/cosign"
+bootstrap_file_with_path=os.path.normpath(f"{ install_dir }/cosign")
 bootstrap_url=f"https://storage.googleapis.com/cosign-releases/{ bootstrap_version }/{ bootstrap_filename }"
 logger.info(f"Downloading bootstrap version '{ bootstrap_version }' of cosign to verify version to be installed")
 download_and_hash(bootstrap_url, bootstrap_file_with_path, sha_to_verify=bootstrap_sha)
@@ -215,7 +215,7 @@ if cosign_release == bootstrap_version:
 # Download specific version of cosign that was requested
 custom_cosign_url=f"https://storage.googleapis.com/cosign-releases/{ cosign_release }/{ desired_cosign_filename }"
 logger.info(f"Downloading requested cosign version '{ cosign_release }'")
-desired_cosign_filename_with_path=f"{ install_dir }/{ desired_cosign_filename }"
+desired_cosign_filename_with_path=os.path.normpath(f"{ install_dir }/{ desired_cosign_filename }")
 sha = download_and_hash(custom_cosign_url, desired_cosign_filename_with_path)
 if sha != bootstrap_sha:
     logger.info(f"Downloading detached signature for platform-specific '{ cosign_release }' release of cosign...")
